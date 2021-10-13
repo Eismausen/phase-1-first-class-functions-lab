4dc81e9c2f5d4ba6e0af843e54c51274faadc6b6
David Avery <eismausen@gmail.com>
2021-10-13 10:23:03 -0700

    commit tree 991cc557bd73a834b5da6734c24881a202393528

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(arr) {
+    return arr.slice(0, 2);
+}
+
+function returnLastTwoDrivers(arr) {
+    return arr.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function (fare) {
+        return fare * multiplier;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(driverArray, driverSelector=returnFirstTwoDrivers) {
+    return driverSelector(driverArray);
+}
